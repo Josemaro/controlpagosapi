39ee281ce4343752a60776d7f704d857fe054d31
Josemaro <josemawo17@gmail.com>
2023-10-20 12:53:13 -0500

    first commit

---
+++ src/main/java/com/sierraazul/controlpagosapi/ControlpagosapiApplication.java
@@ -0,0 +1,13 @@
+package com.sierraazul.controlpagosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlpagosapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlpagosapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sierraazul/controlpagosapi/ControlpagosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sierraazul.controlpagosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlpagosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
